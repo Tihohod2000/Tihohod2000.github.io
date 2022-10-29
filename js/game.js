15576830fa9c0666b97b099c0716725930525cad
Tihohod2000 <116290114+Tihohod2000@users.noreply.github.com>
2022-10-29 15:46:00 +0300

    Обновление 0.009

--- js/game.js
+++ js/game.js
@@ -201,7 +201,7 @@ function draw() {
 // Блок анимарования
 //    requestAnimationFrame(draw);
 //    requestAnimationFrame(setTimeout(draw, 10));
-    setTimeout(draw, 3);
+    setTimeout(draw, 8);
 }
 // Запуск функции после загрузки background
 bg.onload = draw;
