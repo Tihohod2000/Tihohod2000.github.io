c7762471a7883cb17fd1e5b9a5b1c8e79375220d
Tihohod2000 <dumanovskiy15@gmail.com>
2022-10-30 14:47:30 +0300

    Added lvls

--- js/game.js
+++ js/game.js
@@ -38,9 +38,19 @@ game_over.src = "music/game_over.mp3";
 fly.src = "music/fly.mp3";
 othcive.src = "music/othcive.mp3";
 
+//Лучший счёт(Результат)
+var top = localStorage.getItem('top');
 
 //Расстояние между препятствиями
-var gap = 125
+var gap = 125;
+
+
+//Скорость игры
+var fps = 8;
+
+//Переменная повышения сложности
+var lvl = 0
+
 
 // Позиция птички
 var xPos = 40;
@@ -56,38 +66,35 @@ var speed = 2;
 var ball = 0;
 
 // Километров в час 
-var speed_car = 40;
+var speed_car = 30;
 
 //Переменная проигрыша
-var  theEnd = 0
+var theEnd = 0;
+
 
 // Функции полёта
-function moveUp(){
+function moveUp() {
     fly.play();
     grav -= 12;
 }
 
-function moveDown(){
+function moveDown() {
     grav += 3;
 }
 
 // При нажатие на копку
-document.onkeypress = function(event){
-//    console.log(event);
-    if (event.code == "KeyW" || event.which == 1){
-    moveUp();
-    bgMusic.play();
+document.onkeypress = function (event) {
+    //    console.log(event);
+    if (event.code == "KeyW") {
+        moveUp();
+        bgMusic.play();
     }
-    if(event.code == "KeyS"){
-    moveDown();
+    if (event.code == "KeyS") {
+        moveDown();
     }
 }
 
-function sayThanks() {
-    moveUp();
-    bgMusic.play();
-}
-
+//При нажатии на мышь
 onclick = function (event) {
     moveUp();
     bgMusic.play()
@@ -102,15 +109,15 @@ function bomba() {
 }
 
 // Анимация взрыва
-function boomz(){
+function boomz() {
     ctx.drawImage(boom, xPos - 20, yPos - 20);
 }
 
-function boomz2(){
+function boomz2() {
     ctx.drawImage(boom2, xPos - 10, yPos - 20);
 }
 
-function boomz3(){
+function boomz3() {
     ctx.drawImage(boom3, xPos - 15, yPos - 23);
 }
 
@@ -118,101 +125,117 @@ function boomz3(){
 // Массив создания блоков
 var pipe = [];
 pipe[0] = {
-    x : cvs.width,
-    y : -200
+    x: cvs.width,
+    y: -200
 }
 
 
 function draw() {
-//    bgMusic.play();
-    if(theEnd != 0){
+    //    bgMusic.play();
+    if (theEnd != 0) {
         return
     }
     ctx.drawImage(bg, 0, 0, 800, 450);
     ctx.drawImage(bird, xPos, yPos);
 
-// Расположение блоков
-    for(var i = 0; i < pipe.length; i++){
-    ctx.drawImage(pipeUp, pipe[i].x, pipe[i].y)
-    ctx.drawImage(pipeBottom, pipe[i].x, pipe[i].y + pipeUp.height + gap);
-    pipe[i].x -= speed;
+    // Расположение блоков
+    for (var i = 0; i < pipe.length; i++) {
+        ctx.drawImage(pipeUp, pipe[i].x, pipe[i].y)
+        ctx.drawImage(pipeBottom, pipe[i].x, pipe[i].y + pipeUp.height + gap);
+        pipe[i].x -= speed;
 
-// Частота спавна блоков
-    if(pipe[i].x == 154 || pipe[i].x == 155) {
-// Позиция блоков
-    pipe.push({
-    x : cvs.width,
-    y : Math.floor(Math.random() * pipeBottom.height) - pipeBottom.height
-           });
-    }
-
-
-// Условия проигрыша
-    if(xPos + bird.width >= pipe[i].x
-        && xPos <= pipe[i].x + pipeUp.width
-        && (yPos <= pipe[i].y + pipeUp.height || yPos + bird.height >= pipe[i].y + pipeUp.height + gap)
-        || (yPos + bird.height * 1.5 <= 0) || (yPos - bird.height >= cvs.height)){
-        bomba();
-        boomz();
-        setTimeout(boomz2, 90);
-        setTimeout(boomz3, 180);
-        setTimeout(end, 350);
-        theEnd = 1;
-    }
+        // Частота спавна блоков
+        if (pipe[i].x == 154 || pipe[i].x == 155) {
+            // Позиция блоков
+            pipe.push({
+                x: cvs.width,
+                y: Math.floor(Math.random() * pipeBottom.height) - pipeBottom.height
+            });
+        }
 
 
-// Счётчик
-    if(pipe[i].x < 5 && pipe[i].x > 2) {
-    ball += 1;
-    speed_car += 7
-    ball_audio.play();
+        // Условия проигрыша
+        if (xPos + bird.width >= pipe[i].x
+            && xPos <= pipe[i].x + pipeUp.width
+            && (yPos <= pipe[i].y + pipeUp.height || yPos + bird.height >= pipe[i].y + pipeUp.height + gap)
+            || (yPos + bird.height * 1.5 <= 0) || (yPos - bird.height >= cvs.height)) {
+            bomba();
+            boomz();
+            setTimeout(boomz2, 90);
+            setTimeout(boomz3, 180);
+            setTimeout(end, 350);
+            theEnd = 1;
+        }
+
+
+        // Счётчик
+        if (pipe[i].x < 5 && pipe[i].x > 2) {
+            ball += 20;
+            speed_car += 3
+            ball_audio.play();
         }
     }
-    if(ball % 5 == 0 && ball > 0){
-    othcive.play();
+    if (ball % 100 == 0 && ball > 0) {
+        othcive.play();
     }
 
-// Счёт
+
+    //Сложность
+    if (ball % 100 == 0 && ball > 0 && lvl % 5 == 0) {
+        lvl = 1;
+        console.log(lvl);
+        fps -= 1;
+        console.log("Текущий уровень сложности", 9 - fps);
+    }
+
+    if (ball % 100 == 20) {
+        lvl = 0
+        //console.log(lvl)
+    }
+
+
+
+    // Счёт
     ctx.fillStyle = "#fff";
     ctx.font = "20px Verdana";
-    ctx.fillText("Счёт: " +ball, 10, cvs.height - 20);
-// Спидометр
+    ctx.fillText("Счёт: " + ball, 10, cvs.height - 20);
+    // Спидометр
     ctx.fillStyle = "#fff";
     ctx.font = "10px Verdana";
-    ctx.fillText("km/h " +speed_car, xPos + 13, yPos - 5);
+    ctx.fillText("km/h " + speed_car, xPos + 13, yPos - 5);
 
-// Свободное падение
+    // Свободное падение
     yPos += grav;
 
-    if(grav < 2){
-    grav += 1;
+    if (grav < 2) {
+        grav += 1;
     }
-    if(grav > 2){
-    grav -= 1;
-    }
-
-// Пределы скорости взлёта и подения или сопротивление воздуха
-    if(grav > 8){
-    grav = 8
-    }
-    if(grav < -11){
-    grav = -11
+    if (grav > 2) {
+        grav -= 1;
     }
 
-//Созранение лучшего рузельтата в локальную пямать
+    // Пределы скорости взлёта и подения или сопротивление воздуха
+    if (grav > 8) {
+        grav = 8;
+    }
+    if (grav < -11) {
+        grav = -11;
+    }
+
+    //Созранение лучшего рузельтата в локальную пямать
     var top = localStorage.getItem('top');
     top = JSON.parse(top);
-    if (top < ball){
-    localStorage.setItem('top', ball);
+    if (top < ball) {
+        localStorage.setItem('top', ball);
     }
-//Функция конца игры
-    function end(){
+    //Функция конца игры
+    function end() {
         alert("Ты проиграл! Твой счёт ==> " + ball + "  Лучший результат ==> " + top);
         location.reload();
     }
 
-// Блок анимарования
-    setTimeout(draw, 8);
+    // Блок анимарования
+    setTimeout(draw, fps);
 }
 // Запуск функции после загрузки background
 bg.onload = draw;
